cbacb03957bb6f35decfbcd76c0fb6fe75efa52f
NathanielWatife <awosanminathaniel0@gmail.com>
2025-09-29 19:19:04 +0100

    new update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TzslwuDJ+llCdy6eHhXoFyPSFVBMcguYUM3r0so0Uwc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"71NGvYxJmHxQtHSaKgybF5tXPXsTOqz+Cmuy5PBwdpo=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // This creates a standalone output folder
-  images: {
-    unoptimized: true, // Required for static deployment
-  },
+  output: 'export',
   trailingSlash: true,
-  // Remove the experimental.appDir since it's no longer needed in Next.js 14
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
